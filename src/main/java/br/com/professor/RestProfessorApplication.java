5572a5ecb9eb416651d6b3404d46e8ab06569925
Jose Carlos <jotanaval2009@gmail.com>
2017-12-15 20:04:55 -0200

    foi

---
+++ src/main/java/br/com/professor/RestProfessorApplication.java
@@ -0,0 +1,12 @@
+package br.com.professor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestProfessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestProfessorApplication.class, args);
+	}
+}
